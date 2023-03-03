52eca99bd29e4cf3f23a2cc219f059bb8c4fa5da
Rogin Farrer <rfarrer@wayfair.com>
2023-03-03 11:30:25 -0800

    update

--- src/rules/use-client.ts
+++ src/rules/use-client.ts
@@ -1,11 +1,16 @@
 import type { Rule } from "eslint";
 import type {
   Expression,
+  ExpressionStatement,
   Identifier,
   Node,
   Program,
   SpreadElement,
+  Super,
 } from "estree";
+
+const hookRegex = /^use[A-Z]/;
+const useClientRegex = /^('|")use client('|")/;
 
 const meta: Rule.RuleModule["meta"] = {
   docs: {
@@ -17,24 +22,52 @@ const meta: Rule.RuleModule["meta"] = {
   hasSuggestions: true,
 };
 
-function findParentProgram(
-  node: Node & Rule.NodeParentExtension
-): Node & Rule.NodeParentExtension {
+type RealNode = Node & Rule.NodeParentExtension;
+
+function findParentProgram(node: RealNode): Program {
   const parent = node.parent;
-  if (node.parent.type === "Program") {
+  if (parent.type === "Program") {
     return parent;
   }
   return findParentProgram(parent);
 }
 
 const create: Rule.RuleModule["create"] = (context) => {
-  let hasInstance = false;
-  const instances: string[] = [];
+  let hasReported = false;
+  const instances = [];
   let hasDirective = false;
   const sourceCode = context.getSourceCode();
   const firstLine = sourceCode.lines.filter(Boolean)[0].trim();
-  if (/('|")use client('|")/.test(firstLine)) {
+
+  if (useClientRegex.test(firstLine)) {
     hasDirective = true;
+  }
+
+  function reportMissingDirective(
+    node: Node & Rule.NodeParentExtension,
+    expression: Identifier | Expression | Super
+  ) {
+    hasReported = true;
+    context.report({
+      node: expression,
+      message:
+        "Using hooks requires that this file have the 'use client' directive at the top of the file.",
+      // suggest: [
+      //   {
+      //     desc: "add use client at the top of the document",
+      //     fix(fixer) {
+      //       const p = findParentProgram(node.parent);
+      //       const firstToken = sourceCode.getFirstToken(p.body[0]);
+      //       return fixer.insertTextBefore(firstToken!, `'use client';\n\n`);
+      //     },
+      //   },
+      // ],
+      *fix(fixer) {
+        const p = findParentProgram(node.parent);
+        const firstToken = sourceCode.getFirstToken(p.body[0]);
+        yield fixer.insertTextBefore(firstToken!, `'use client';\n`);
+      },
+    });
   }
 
   return {
@@ -42,29 +75,13 @@ const create: Rule.RuleModule["create"] = (context) => {
       const declarator = node.declarations[0];
       if (declarator.init && declarator.init.type === "CallExpression") {
         const expression = declarator.init;
-        if (expression.callee.name === "useState") {
-          hasInstance = true;
-          instances.push("useState");
-          if (hasDirective) {
+        const name = "name" in expression.callee ? expression.callee.name : "";
+        if (hookRegex.test(name)) {
+          instances.push(name);
+          if (hasDirective || hasReported) {
             return;
           }
-          context.report({
-            node: expression.callee,
-            message: "write `use client`",
-            suggest: [
-              {
-                desc: "add use client at the top of the document",
-                fix(fixer) {
-                  const p = findParentProgram(node.parent);
-                  return fixer.insertTextBefore(p, "'use client';");
-                },
-              },
-            ],
-            // *fix(fixer) {
-            //   const p = findParentProgram(node.parent);
-            //   yield [fixer.insertTextBefore(p, "'use client';")];
-            // },
-          });
+          reportMissingDirective(node, expression.callee);
         }
       }
     },
@@ -73,39 +90,42 @@ const create: Rule.RuleModule["create"] = (context) => {
         callee?: Identifier;
         arguments?: Array<Expression | SpreadElement>;
       };
-      if (!expression.callee) return;
-      const name = expression.callee.name;
-      if (name === "useEffect" || name === "useLayoutEffect") {
-        hasInstance = true;
-        instances.push(name);
-        context.report({
-          node: expression.callee,
-          message: "write `use client`",
-          suggest: [
-            {
-              desc: "add use client at the top of the document",
-              fix(fixer) {
-                const p = findParentProgram(node.parent);
-                return fixer.insertTextBefore(p, "'use client';");
-              },
-            },
-          ],
-        });
+      if (!expression.callee) {
+        return;
+      }
+
+      if (expression.callee && hookRegex.test(expression.callee.name)) {
+        instances.push(expression.callee.name);
+        if (hasDirective || hasReported) {
+          return;
+        }
+        reportMissingDirective(node, expression.callee);
       }
     },
-    "Program:exit": (node: Program) => {
-      if (instances.length < 1 && hasDirective) {
+
+    "ExpressionStatement:exit"(
+      node: ExpressionStatement & Rule.NodeParentExtension
+    ) {
+      const value = "value" in node.expression ? node.expression.value : "";
+      if (typeof value !== "string" || !useClientRegex.test(value)) {
+        return;
+      }
+      if (instances.length === 0 && hasDirective) {
         context.report({
           node,
-          message: "remove `use client`",
-          suggest: [
-            {
-              desc: "remove use client at the top of the document",
-              fix(fixer) {
-                return fixer.remove(node.body[0]);
-              },
-            },
-          ],
+          message:
+            "This file is not using hooks, therefore it should not have the 'use client' directive.",
+          fix(fixer) {
+            return fixer.remove(node);
+          },
+          // suggest: [
+          //   {
+          //     desc: "remove use client at the top of the document",
+          //     fix(fixer) {
+          //       return fixer.remove(node);
+          //     },
+          //   },
+          // ],
         });
       }
     },
